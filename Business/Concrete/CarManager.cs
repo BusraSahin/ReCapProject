ce9dc439fb0e49b9f0390f0ac2df3a559bd9cfac
0614busra.bra@gmail.com <0614busra.bra@gmail.com>
2021-04-07 22:17:47 +0300

    .

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,32 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if(car.DailyPrice>0 && car.Description.Length >= 2)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Operation Succesfull");
+            }
+            else
+            {
+                Console.WriteLine("Operation Failed");
+            }
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
         }
     }
 }
